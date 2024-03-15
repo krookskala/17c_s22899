42bf7985d7ee33fb9bc5e015f2bfc8766ca78ae9
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-16 00:52:30 +0100

    Task3 Done.

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ start = 1
 end = 10
 squares = generates_list_of_squares(start, end)
 print("List Of Squares From {} To {}: {}".format(start, end, squares))
+
+#Task3
+class SquareGenerator:
+    def generate_list_of_squares(self, start, end):
+        squares = [num ** 2 for num in range(start, end + 1)]
+        return squares
+if __name__ == "__main__":
+    square_gen = SquareGenerator()
+
+    start = 1
+    end = 10
+    squares = square_gen.generate_list_of_squares(start, end)
+    print("List Of Squares From {} To {}: {}".format(start, end, squares))
+
