0e77ba91fe7e02ef7306baa8183ebd3dea75f42d
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-16 00:45:01 +0100

    Task2 Done.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 #Task1
 squares = [num ** 2 for num in range(1, 11)]
 print("List Of Squares From 1 To 10:", squares)
+
+#Task2
+def generates_list_of_squares(start, end):
+    squares = [num ** 2 for num in range(start, end + 1)]
+    return squares
+
+start = 1
+end = 10
+squares = generates_list_of_squares(start, end)
+print("List Of Squares From {} To {}: {}".format(start, end, squares))
