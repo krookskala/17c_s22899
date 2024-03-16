31acc1a9d65cce66a4577e887f5c14c7760cd209
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-16 01:45:03 +0100

    Task10 Done.

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -49,3 +49,24 @@ try:
     print("List Of Cubes From {} To {}: {}".format(start, end, cubes))
 except ValueError as e:
     print("Error: ", e)
+
+#Task10
+from abc import ABC, abstractmethod
+class SquareGenerator(ABC):
+    @abstractmethod
+    def generate_list_of_squares(self, start, end):
+        pass
+class CubicGenerator(SquareGenerator):
+    def generate_list_of_squares(self, start, end):
+        if start > end:
+            raise ValueError("Start Of The Range Must Be Less Than Or Equal To The End.")
+        cubes = [num ** 3 for num in range(start, end + 1)]
+        return cubes
+cubic_gen = CubicGenerator()
+start = 1
+end = 5
+try:
+    cubes = cubic_gen.generate_list_of_squares(start, end)
+    print("List Of Cubes From {} To {}: {}".format(start, end, cubes))
+except ValueError as e:
+    print("Error:", e)
