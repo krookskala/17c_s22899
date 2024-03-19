522640aa276c8d4f02fc484243f78037dc7ce5ed
Ismail Sariarslan <ismailsariarslan7@gmail.com>
2024-03-19 21:12:13 +0100

    CREATED README.md

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,12 +1,10 @@
 import math
-
 class SquareGenerator:
     def generate_list_of_squares(self, start, end):
         if end < start:
             raise ValueError("End Of The Range Must Be Greater Than Or Equal To The Start.")
         squares = [num ** 2 for num in range(start, end + 1)]
         return squares
-
 square_gen = SquareGenerator()
 start = 1
 end = 10
@@ -15,7 +13,6 @@ try:
     print("List Of Squares From {} To {}: {}".format(start, end, squares))
 except ValueError as e:
     print("Error:", e)
-
 #Task8
 class CubicGenerator(SquareGenerator):
     def generate_list_of_squares(self, start, end):
@@ -23,7 +20,6 @@ class CubicGenerator(SquareGenerator):
             raise ValueError("End Of The Range Must Be Greater Than Or Equal To The Start.")
         cubes = [num ** 3 for num in range(start, end + 1)]
         return cubes
-
 cubic_gen = CubicGenerator()
 start = 1
 end = 5
@@ -40,7 +36,6 @@ class CubicGenerator(SquareGenerator):
             raise ValueError("Start Of The Range Must Be Greater Than Or Equal To The End.")
         cubes = [num ** 3 for num in range(start, end + 1)]
         return cubes
-
 cubic_gen = CubicGenerator()
 start = 1
 end = 5
